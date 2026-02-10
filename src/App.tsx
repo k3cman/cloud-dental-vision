27f306fc8ecd3656eee43c804681650fe353f9a4
k3cman <kecman.dev@gmail.com>
2026-02-10 15:07:08 +0100

    Fix workflow

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* Use basename so React Router works under GitHub Pages project path */}
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
